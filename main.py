ec679c9cd98885290010a5ae3603ed2f21de9499
speculate56 <203856883+speculate56@users.noreply.github.com>
2025-03-25 09:16:55 -0400

    Update main.py
    - Added documentation for each function

--- main.py
+++ main.py
@@ -22,16 +22,24 @@ class Main:
                 words.add(word.lower())
         return list(words)
 
-    def crosswordSolver(self, parameter: str) -> list:
+    def crosswordSolver(self, pattern: str) -> list:
+        """
+        Returns a list of words from a wordlist that match the given pattern.
+    
+        The pattern can include '?' as a wildcard that matches any letter.
+    
+        @param pattern: The pattern to match, with '?' as a wildcard.
+        @return: A list of valid words matching the pattern, in lowercase.
+        """
         valid_words = set()
 
         for word in self.words:
-            if len(word) != len(parameter):
+            if len(word) != len(pattern):
                 continue
 
             match = True
-            for i in range(len(parameter)):
-                if parameter[i] != '?' and parameter.lower()[i] != word.lower()[i]:
+            for i in range(len(pattern)):
+                if pattern[i] != '?' and pattern.lower()[i] != word.lower()[i]:
                     match = False
                     break
             if match:
@@ -39,6 +47,12 @@ class Main:
         return list(valid_words)
 
     def minusOne(self, word: str) -> list:
+        """
+        Returns a list of words from a wordlist that can be formed by removing one character from `word`.
+    
+        @param word: The word from which a character will be removed.
+        @return: A list of valid words formed by removing one character from the input word, in lowercase.
+        """
         valid_words = set()
         word_variations = set()
 
@@ -51,13 +65,19 @@ class Main:
         return list(valid_words)
 
     def plusOne(self, word: str) -> list:
+        """
+        Returns a list of words from a wordlist that can be formed by adding one letter to `word`.
+    
+        @param word: The word to which a single letter will be added.
+        @return: A list of valid words formed by adding one letter to the input word.
+        """
         valid_words = set()
 
         for letter in 'abcdefghijklmnopqrstuvwxyz':
             for i in range(len(word) + 1):
                 altered_word = word.lower()[:i] + letter + word.lower()[i:]
 
-                if altered_word in self.words:  # words are not converted to lowercase
+                if altered_word in self.words:
                     valid_words.add(altered_word)
         return list(valid_words)
 
